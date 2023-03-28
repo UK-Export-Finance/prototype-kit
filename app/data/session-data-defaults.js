69ce7bebe6937f4bf52659f0c2ba4053bd5a5639
dap-protos <108474367+tom-decs@users.noreply.github.com>
2023-03-28 22:45:47 +0100

    Facility 2 updates
    Moved pages into a folder
    Made all the updatable fields dynamic
    General tidying

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,8 +1,19 @@
 module.exports = {
 
   // Insert values here
-  'facility-2-updated': '',
+  
+  // Dates
   '1-month-ago': '28 Feb 2023',
   'todays-date': '29 Mar 2023',
-  'tomorrow': '28 Mar 2023'
+  'tomorrow': '28 Mar 2023',
+  
+  // FACILITY 2 VALUES
+  'facility-2-updated': '',
+  'facility-2-utilisation': '125,000',
+  'facility-2-exposure': '41,666.67',
+  'facility-2-accrued': '95.64',
+  'facility-2-paid': '79.23',
+  
+  
+
 }
