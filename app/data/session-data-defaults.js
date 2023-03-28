03823cd1412665f2c5b3a1c0e5ea63910338c77d
dap-protos <108474367+tom-decs@users.noreply.github.com>
2023-03-28 17:39:21 +0100

    Updates

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,7 +1,8 @@
 module.exports = {
 
   // Insert values here
+  'facility-2-updated': '',
   '1-month-ago': '28 Feb 2023',
-  'todays-date': '27 Mar 2023',
+  'todays-date': '29 Mar 2023',
   'tomorrow': '28 Mar 2023'
 }
