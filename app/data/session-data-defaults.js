35cbc67dea7e59be6fe001031ac72dd0e717c4f9
dap-protos <108474367+tom-decs@users.noreply.github.com>
2023-03-29 12:18:53 +0100

    Added all 4 facilities

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -7,13 +7,39 @@ module.exports = {
   'todays-date': '29 Mar 2023',
   'tomorrow': '28 Mar 2023',
   
+  // FACILITY 1 VALUES
+  'facility-1-updated': '',
+  'facility-1-name': 'COMPRESSORS LTD',
+  'facility-1-ref': '0020001371',
+  'facility-1-utilisation': '125,000.00',
+  'facility-1-exposure': '41,666.67',
+  'facility-1-accrued': '95.64',
+  'facility-1-paid': '79.23',
+
   // FACILITY 2 VALUES
   'facility-2-updated': '',
-  'facility-2-utilisation': '125,000',
+  'facility-2-name': 'PETROLEUM LTD',
+  'facility-2-ref': '0020001413',
+  'facility-2-utilisation': '125,000.00',
   'facility-2-exposure': '41,666.67',
   'facility-2-accrued': '95.64',
   'facility-2-paid': '79.23',
-  
-  
 
+  // FACILITY 3 VALUES
+  'facility-3-updated': '',
+  'facility-3-name': 'PHARAMACUETICALS LTD',
+  'facility-3-ref': '0020001467',
+  'facility-3-utilisation': '125,000.00',
+  'facility-3-exposure': '41,666.67',
+  'facility-3-accrued': '95.64',
+  'facility-3-paid': '79.23',
+
+  // FACILITY 4 VALUES
+  'facility-4-updated': '',
+  'facility-4-name': 'XYZ MOTORS LTD',
+  'facility-4-ref': '	0020001499',
+  'facility-4-utilisation': '125,000.00',
+  'facility-4-exposure': '41,666.67',
+  'facility-4-accrued': '95.64',
+  'facility-4-paid': '79.23',
 }
