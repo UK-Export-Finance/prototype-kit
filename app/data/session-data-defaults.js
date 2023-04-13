3fdeb24e947580649984f7a6a27ec72ea7b491a9
dap-protos <108474367+tom-decs@users.noreply.github.com>
2023-04-13 17:12:55 +0100

    Added more facilities

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -9,9 +9,10 @@ module.exports = {
   'last-month': 'February 2023',
   'report-month': 'March 2023',
   '1-month-ago': '3 March 2023',
-  'todays-date': '12 April 2023',
-  'todays-date-full': '11 April 2023',
-  'next-month-report-due': '10 May 2023',
+  'todays-date': '13 April 2023',
+  'todays-date-full': '13 April 2023',
+  'this-month-report-due': '18 April 2023',
+  'next-month-report-due': '15 May 2023',
   
   // Names
   'signed-in-name': 'John Smith',
@@ -19,11 +20,12 @@ module.exports = {
 
   // FACILITY 1 VALUES
   'facility-1-updated': 'no',
-  'facility-1-name': 'Compressors LTD',
+  'facility-1-name': 'Compressors Ltd',
   'facility-1-ukef-id': '0020001371',
   'facility-1-ref': '0239397',
+  
   'facility-1-end-date': '16 June 2024',
-  'facility-1-prob-default': 'Reported in June',
+  'facility-1-prob-default': '1.0%',
   'facility-1-limit': '600,000.00',
   'facility-1-cover': '80%',
   'facility-1-max': '480,000.00',
@@ -31,7 +33,7 @@ module.exports = {
   'facility-1-utilisation': '400,000.00',
   'facility-1-exposure': '320,000.00',
   'facility-1-paid': '1,000.00',
-  'facility-1-accrued': '15,000.00',
+  'facility-1-accrued': '10,000.00',
  
   'facility-1new-utilisation': '',
   'facility-1new-exposure': '',
@@ -43,42 +45,90 @@ module.exports = {
   'facility-1accrued-updated': 'no',
 
   // FACILITY 2 VALUES
-  'facility-2-updated': '',
-  'facility-2-name': 'Petroleum LTD',
+  'facility-2-updated': 'no',
+  'facility-2-name': 'Petroleum Ltd',
   'facility-2-ukef-id': '0020001413',
   'facility-2-ref': '0374289',
+
+  'facility-2-end-date': '18 September 2026',
+  'facility-2-prob-default': '1.2%',
+  'facility-2-limit': '300,000.00',
+  'facility-2-cover': '80%',
+  'facility-2-max': '240,000.00',
+
   'facility-2-utilisation': '125,000.00',
-  'facility-2-exposure': '41,666.67',
-  'facility-2-accrued': '95.64',
-  'facility-2-paid': '79.23',
+  'facility-2-exposure': '100,000.00',
+  'facility-2-accrued': '3000.00',
+  'facility-2-paid': '800.00',
+
+  'facility-2new-utilisation': '',
+  'facility-2new-exposure': '',
+  'facility-2new-paid': '',
+  'facility-2new-accrued': '',
+
+  'facility-2utiisation-updated': 'no',
+  'facility-2paid-updated': 'no',
+  'facility-2accrued-updated': 'no',
 
   // FACILITY 3 VALUES
-  'facility-3-updated': '',
-  'facility-3-name': 'Pharmaceuticals LTD',
+  'facility-3-updated': 'no',
+  'facility-3-name': 'Pharmaceuticals Ltd',
   'facility-3-ukef-id': '0020001467',
   'facility-3-ref': '0563982',
-  'facility-3-utilisation': '790,000.00',
-  'facility-3-exposure': '632,000.00',
-  'facility-3-accrued': '727.46',
-  'facility-3-paid': '639.11',
+  
+  'facility-3-end-date': '16 February 2025',
+  'facility-3-prob-default': '0.1%',
+  'facility-3-limit': '800,000.00',
+  'facility-3-cover': '80%',
+  'facility-3-max': '640,000.00',
+  
+  'facility-3-utilisation': '200,000.00',
+  'facility-3-exposure': '160,000.00',
+  'facility-3-accrued': '50,000.00',
+  'facility-3-paid': '2,000.00',
+
+  'facility-3new-utilisation': '',
+  'facility-3new-exposure': '',
+  'facility-3new-paid': '',
+  'facility-3new-accrued': '',
+
+  'facility-3utiisation-updated': 'no',
+  'facility-3paid-updated': 'no',
+  'facility-3accrued-updated': 'no',
 
   // FACILITY 4 VALUES
-  'facility-4-updated': '',
-  'facility-4-name': 'M5 Motors LTD',
+  'facility-4-updated': 'no',
+  'facility-4-name': 'M5 Motors Ltd',
   'facility-4-ref': '	0653289',
   'facility-4-ukef-id': '	0020001499',
-  'facility-4-utilisation': '50,000.00',
-  'facility-4-exposure': '40,000.00',
-  'facility-4-accrued': '84.54',
-  'facility-4-paid': '56.24',
+
+  'facility-4-end-date': '23 Mar 2025',
+  'facility-4-prob-default': '0.7%',
+  'facility-4-limit': '1,500,000.00',
+  'facility-4-cover': '80%',
+  'facility-4-max': '1,500,000.00',
+
+  'facility-4-utilisation': '500,000.00',
+  'facility-4-exposure': '400,000.00',
+  'facility-4-accrued': '20,000.00',
+  'facility-4-paid': '2,000.00',
+
+  'facility-4new-utilisation': '',
+  'facility-4new-exposure': '',
+  'facility-4new-paid': '',
+  'facility-4new-accrued': '',
+
+  'facility-4utiisation-updated': 'no',
+  'facility-4paid-updated': 'no',
+  'facility-4accrued-updated': 'no',
 
   // FACILITY 5 VALUES
   'facility-5-updated': 'no',
-  'facility-5-name': 'A1 Manifolds LTD',
+  'facility-5-name': 'A1 Manifolds Ltd',
   'facility-5-ukef-id': '0020001507',
   'facility-5-ref': '0378429',
   'facility-5-end-date': '23 July 2025',
-  'facility-5-prob-default': 'Reported in July',
+  'facility-5-prob-default': '0.3%',
 
   'facility-5-limit': '2,500,000.00',
   'facility-5-cover': '80%',
@@ -86,8 +136,8 @@ module.exports = {
 
   'facility-5-utilisation': '1,900,000.00',
   'facility-5-exposure': '1,520,000.00',
-  'facility-5-accrued': '1,589.23',
-  'facility-5-paid': '1,724.36',
+  'facility-5-accrued': '50,000.00',
+  'facility-5-paid': '5,000.00',
 
   'facility-5new-utilisation': '',
   'facility-5new-exposure': '1,600,000.00',
@@ -96,7 +146,7 @@ module.exports = {
 
 // FACILITY 6 VALUES
 'facility-6-updated': 'no',
-'facility-6-name': 'XYZ Aircrafts LTD',
+'facility-6-name': 'XYZ Aircrafts Ltd',
 'facility-6-ukef-id': '0020001467',
 'facility-6-ref': '0529012',
 'facility-6-utilisation': '790,000.00',
